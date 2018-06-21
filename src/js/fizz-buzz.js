297f11a50a50f843e32bcf55b117ea81b3896dd7
tochman <thomas@craftacademy.se>
2018-06-21 11:20:51 +0200

    specs for all game rules are passing

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -1,5 +1,14 @@
 function FizzBuzz() {
     this.check = (number) => {
-        return number; 
+        if (number % 15 === 0) {
+            return 'fizz-buzz';
+        } else if (number % 5 === 0) {
+            return 'buzz';
+        } else if (number % 3 === 0) {
+            return 'fizz';
+        } else {
+            return number;
+
+        }
     }
 }
